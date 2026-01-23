a63fa9e1ee195ba9572bc069fc2e665c2db50460
Thamsanqa Hadebe <thamsanqahadebe98@gmail.com>
2026-01-23 11:57:50 +0200

    new: solved dictionary merge

--- assessment2/assessment2/main.py
+++ assessment2/assessment2/main.py
@@ -51,7 +51,19 @@ def merge_dictionaries(dict1: Dict, dict2: Dict) -> Dict:
         Input → dict1 = {"a": 1, "b": 2}, dict2 = {"b": 3, "c": 4}
         Output → {"a": 1, "b": 5, "c": 4}
     """
-    pass
+    result_dict = {}
+    for key, value in dict1.items():
+        if key in result_dict:
+            result_dict[key] += value
+        else:
+            result_dict[key] = value
+    
+    for key, value in dict2.items():
+        if key in result_dict:
+            result_dict[key] += value
+        else:
+            result_dict[key] = value
+    return result_dict
 
 
 
@@ -114,3 +126,4 @@ if __name__ == "__main__":
     # Example:
     # print(sum_numbers(10))
     print(is_anagram("silent", "listen"))
+    print(merge_dictionaries({"a": 1, "b": 2}, {"b": 3, "c": 4}))
